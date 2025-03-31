fbf6db0509e2a10c0ddef9b5f1bf7fd7e514ffa7
Philippe Beaumont <philippe.beaumont@nfrance.com>
2025-04-01 00:48:47 +0200

    near to work

---
+++ pmacontrol/script/init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+/usr/local/bin/php App/Webroot/index.php install webroot /tmp/config.json
+/usr/local/bin/php App/Webroot/index.php install index /tmp/config.json
+/usr/local/bin/php App/Webroot/index.php install createOrganisation /tmp/config.json
+/usr/local/bin/php App/Webroot/index.php install createAdmin /tmp/config.json
+/usr/local/bin/php App/Webroot/index.php ldap updateFromInstall /tmp/config.json
+/usr/local/bin/php App/Webroot/index.php webservice addAccount /tmp/config.json
+/usr/local/bin/php App/Webroot/index.php control createTsTable 
+/usr/local/bin/php App/Webroot/index.php agent updateServerList 
+/usr/local/bin/php App/Webroot/index.php administration generate_model 
+/usr/local/bin/php App/Webroot/index.php administration admin_table
+
+apache2-foreground
