30b805a4b7f7ac1278f4413800107fa9e74e6ce6
Philippe BEAUMONT <philippe.beaumont@nfrance.com>
2025-04-02 18:44:23 +0200

    rewrite some part of rebuild script

--- force_rebuild_all.sh
+++ force_rebuild_all.sh
@@ -1,7 +1,6 @@
-docker stop $(docker ps -aq)
-docker rm $(docker ps -aq)
-docker volume rm $(docker volume ls -q)
+#!/bin/bash
 docker compose --profile pmacontrol down
-docker rmi $(docker images -q)
+docker volume rm pmacontrol-docker_config pmacontrol-docker_db pmacontrol-docker_tmpdb pmacontrol-docker_tmplog
+docker rmi pmacontrol-docker-pmacontrol pmacontrol-docker-mariadb
 docker builder prune -af
 docker compose --profile pmacontrol up -d --build --force-recreate
